1b97c1141ca244ed610e247baee382aeff77061f
Anik Kumar Chakrabortty <hello@anik-online.com>
2017-07-21 07:26:39 +0600

    Add files via upload

---
+++ uri 1760 - Koch Snowflake.cpp
@@ -0,0 +1,25 @@
+/* 
+URI 1760 - Koch Snowflake
+
+Author: Anik Chakrabortty
+Created : July 21, 2017
+Southeast University, Bangladesh (45)
+
+*/ 
+
+#include <bits/stdc++.h>
+
+using namespace std; 
+
+int main(){
+
+double n; 
+
+while(cin >> n){
+	
+	double form = 2 * sqrt(3) * (n*n) / 5; 
+
+	printf("%.2f\n", form); 
+	}
+
+}
